80857e41da8bf27552e4da93a8adaf977a2c17fc
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2022-10-18 19:21:15 +0600

    news json add

--- index.js
+++ index.js
@@ -6,12 +6,16 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 
 const categories = require("./data/categories.json");
+const news = require("./data/news.json");
 
 app.get("/", (req, res) => {
   res.send("hello babu");
 });
 app.get("/news-categories", (req, res) => {
   res.send(categories);
+});
+app.get("/news", (req, res) => {
+  res.send(news);
 });
 
 app.listen(port, () => {
