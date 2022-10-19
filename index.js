a2b622505291b22fce57b3db36e4f2f4e0324bbb
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2022-10-19 22:06:34 +0600

    category_id

--- index.js
+++ index.js
@@ -14,8 +14,17 @@ app.get("/", (req, res) => {
 app.get("/news-categories", (req, res) => {
   res.send(categories);
 });
-app.get("/news", (req, res) => {
-  res.send(news);
+
+app.get("/category/:id", (req, res) => {
+  const id = req.params.id;
+  const category_news = news.filter((e) => e.category_id === id);
+  res.send(category_news);
+});
+
+app.get("/news/:id", (req, res) => {
+  const id = req.params.id;
+  const selectedNews = news.find((e) => e._id === id);
+  res.send(selectedNews);
 });
 
 app.listen(port, () => {
